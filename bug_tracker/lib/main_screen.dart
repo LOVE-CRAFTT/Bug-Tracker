54488b843da0435c6070e3bce06808a892fc691e
Brendan Chukwuemeka <chukwuemekachukwudi9@gmail.com>
2023-10-22 16:13:42 +0300

    refactor page switching process

--- bug_tracker/lib/main_screen.dart
+++ bug_tracker/lib/main_screen.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:bug_tracker/ui_components/custom_navigation_rail.dart';
-import 'package:bug_tracker/pages/home_page.dart';
-import 'package:bug_tracker/pages/feed_page.dart';
-import 'package:bug_tracker/pages/discuss_page.dart';
-import 'package:bug_tracker/pages/calendar_page.dart';
-import 'package:bug_tracker/pages/projects_page.dart';
-import 'package:bug_tracker/pages/bugs_page.dart';
-import 'package:bug_tracker/pages/milestones_page.dart';
-import 'package:bug_tracker/pages/timesheets_page.dart';
+import 'package:bug_tracker/utilities/page_functions.dart';
 
 ///Contains a Static Navigation Rail and changeable pages
 class MainScreen extends StatefulWidget {
@@ -19,35 +12,9 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   late Widget page;
-
-  ///Process to choose page based on currently selected destination
-  ///Is run everytime new destination is selected
-  void selectPage() {
-    switch (selectedIndex) {
-      case 0:
-        page = const HomePage();
-      case 1:
-        page = const FeedPage();
-      case 2:
-        page = const DiscussPage();
-      case 3:
-        page = const CalendarPage();
-      case 4:
-        page = const ProjectsPage();
-      case 5:
-        page = const BugsPage();
-      case 6:
-        page = const MilestonesPage();
-      case 7:
-        page = const TimesheetsPage();
-      default:
-        page = const HomePage();
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    selectPage();
+    page = selectPage();
     return Scaffold(
       body: Row(
         mainAxisSize: MainAxisSize.min,
---
+++ bug_tracker/lib/utilities/page_functions.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:bug_tracker/ui_components/custom_navigation_rail.dart';
+import 'package:bug_tracker/pages/home_page.dart';
+import 'package:bug_tracker/pages/feed_page.dart';
+import 'package:bug_tracker/pages/discuss_page.dart';
+import 'package:bug_tracker/pages/calendar_page.dart';
+import 'package:bug_tracker/pages/projects_page.dart';
+import 'package:bug_tracker/pages/bugs_page.dart';
+import 'package:bug_tracker/pages/milestones_page.dart';
+import 'package:bug_tracker/pages/timesheets_page.dart';
+
+///Process to choose page based on currently selected destination
+///Is run everytime new destination is selected
+Widget selectPage() {
+  switch (selectedIndex) {
+    case 0:
+      return const HomePage();
+    case 1:
+      return const FeedPage();
+    case 2:
+      return const DiscussPage();
+    case 3:
+      return const CalendarPage();
+    case 4:
+      return const ProjectsPage();
+    case 5:
+      return const BugsPage();
+    case 6:
+      return const MilestonesPage();
+    case 7:
+      return const TimesheetsPage();
+    default:
+      return const HomePage();
+  }
+}
