b4db94ecd1af44a5b0ce4c4e307157b95365f4ae
Brendan Chukwuemeka <chukwuemekachukwudi9@gmail.com>
2023-10-27 17:27:51 +0300

    Add text field to start discussions

--- bug_tracker/lib/pages/feed_page.dart
+++ bug_tracker/lib/pages/feed_page.dart
@@ -1,3 +1,4 @@
+import 'package:bug_tracker/utilities/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:bug_tracker/ui_components/appbar.dart';
 import 'package:bug_tracker/ui_components/feed_page_choice_button.dart';
@@ -11,6 +12,8 @@ class FeedPage extends StatefulWidget {
 
 class _FeedPageState extends State<FeedPage> {
   String? dropDownValue = choices.first;
+  final biggerSpacingHeight = 50.0;
+  final smallerSpacingHeight = 30.0;
 
   @override
   Widget build(BuildContext context) {
@@ -18,16 +21,61 @@ class _FeedPageState extends State<FeedPage> {
       appBar: reusableAppBar("Feed"),
       body: ListView(
         children: [
-          Column(
+          FeedChoiceButton(
+            dropDownValue: dropDownValue,
+            onChanged: (selected) {
+              setState(() {
+                dropDownValue = selected;
+              });
+            },
+          ),
+          SizedBox(
+            height: biggerSpacingHeight,
+          ),
+          Row(
             children: [
-              FeedChoiceButton(
-                  dropDownValue: dropDownValue,
-                  onChanged: (selected) {
-                    setState(() {
-                      dropDownValue = selected;
-                    });
-                  }),
+              const Padding(
+                padding: EdgeInsets.only(left: 15, right: 20),
+                child: Material(
+                  shape: CircleBorder(),
+                  color: Colors.grey,
+                  child: IconButton(
+                    onPressed: null,
+                    tooltip: "ChukwuemekaChukwudi9",
+                    icon: Icon(Icons.account_circle_outlined),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constraints) {
+                    var rightPadding =
+                        constraints.maxWidth > 550 ? 150.0 : 50.0;
+                    return Padding(
+                      padding: EdgeInsets.only(right: rightPadding),
+                      child: TextField(
+                        style: kContainerTextStyle.copyWith(
+                          color: Colors.black,
+                        ),
+                        decoration: InputDecoration(
+                          hintText: "Start a discussion",
+                          hintStyle: kContainerTextStyle.copyWith(
+                              color: Colors.black45),
+                          border: const OutlineInputBorder(),
+                          filled: true,
+                          fillColor: Colors.white70,
+                        ),
+                        onChanged: (text) {},
+                        onSubmitted: (submitText) {},
+                      ),
+                    );
+                  },
+                ),
+              ),
             ],
+          ),
+          const SizedBox(
+            height: 1200,
           )
         ],
       ),
