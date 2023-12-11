54b2dd522ac3891f130c51a08dd821a7eeaa5bc4
Brendan Chukwuemeka <chukwuemekachukwudi9@gmail.com>
2023-12-11 20:39:36 +0200

    differentiate table types

---
+++ bug_tracker/lib/utilities/build_complex_table_row.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:bug_tracker/utilities/constants.dart';
+
+/// Build Table Rows in the discuss page
+/// Parameters are all named and optional and they include
+/// [firstHeader] => the header of the first column
+/// [secondHeader] => the header of the second column
+/// [thirdHeader] => the header of the first column
+/// conversationTitle, projectName, tooltipMessage and avatarText and the backgroundImage
+TableRow buildTableRow({
+  String? firstHeader,
+  String? secondHeader,
+  String? thirdHeader,
+  String? conversationTitle,
+  String? projectName,
+  List<String>? avatarText,
+  List<String>? tooltipMessage,
+  String? backgroundImage,
+}) {
+  TextStyle cellTextStyle = kContainerTextStyle.copyWith(fontSize: 14.0);
+
+  return TableRow(
+    children: [
+      ListTile(
+        /// Creates an Icon button, conversation title and makes it clickable if it is not the first column title
+        /// else it is just text containing the first header
+        leading: firstHeader == null
+            ? IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.settings,
+                  color: secondaryThemeColor,
+                  size: 15.0,
+                ),
+              )
+            : null,
+        title: Text(firstHeader ?? (conversationTitle ?? "Null Value")),
+        titleTextStyle: cellTextStyle,
+        onTap: firstHeader == null ? () {} : null,
+      ),
+      ListTile(
+        /// Creates a project name and makes it clickable if it not the second column title
+        /// else it is just text containing the second header
+        title: Text(secondHeader ?? (projectName ?? "Error Value")),
+        titleTextStyle: cellTextStyle,
+        onTap: secondHeader == null ? () {} : null,
+      ),
+      ListTile(
+        /// Creates a list of circle avatars if it is not the third column title
+        /// else it just text containing the third header
+        title: thirdHeader == null
+            ? SizedBox(
+                height: 50.0,
+                child: ListView.builder(
+                  itemCount: avatarText?.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (BuildContext context, int index) {
+                    final String? lAvatarMessage = avatarText?[index];
+                    final String? lTooltipMessage = tooltipMessage?[index];
+                    return Padding(
+                      padding: const EdgeInsets.only(right: 4.0),
+                      child: Tooltip(
+                        message: lTooltipMessage,
+                        child: avatarText != null
+                            ? CircleAvatar(
+                                backgroundColor: Colors.grey,
+                                child: lAvatarMessage != null
+                                    ? Text(lAvatarMessage)
+                                    : const Text("Error Value"),
+                              )
+                            : null,
+                      ),
+                    );
+                  },
+                ),
+              )
+            : Text(thirdHeader),
+        titleTextStyle: cellTextStyle,
+      )
+    ],
+  );
+}
