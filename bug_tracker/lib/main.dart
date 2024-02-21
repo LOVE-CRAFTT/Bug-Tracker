489dba3458952ccc9127d854ceb8b7e9105750d2
Brendan Chukwuemeka <chukwuemekachukwudi9@gmail.com>
2024-02-21 14:31:38 +0100

    Update main.dart

--- bug_tracker/lib/main.dart
+++ bug_tracker/lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:bug_tracker/models/overview.dart';
-import 'package:bug_tracker/user_pages/user_main_page.dart';
 import 'package:bug_tracker/sign_in.dart';
 
 void main() {
@@ -33,8 +32,7 @@ class MyApp extends StatelessWidget {
         },
       ),
       title: 'Bug tracker',
-      // home: SignInPage(),
-      home: const UserMainPage(),
+      home: SignInPage(),
     );
   }
 }
