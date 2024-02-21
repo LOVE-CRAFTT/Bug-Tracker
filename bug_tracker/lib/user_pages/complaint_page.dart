2cc43af946d00d2cf56c0256c77bd35b3c905297
Brendan Chukwuemeka <chukwuemekachukwudi9@gmail.com>
2024-02-21 14:28:41 +0100

    add complaints note and finish user UI

--- bug_tracker/lib/user_pages/complaint_page.dart
+++ bug_tracker/lib/user_pages/complaint_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:bug_tracker/utilities/constants.dart';
+import 'package:bug_tracker/utilities/build_complaint_notes.dart';
 
 class ComplaintPage extends StatelessWidget {
   const ComplaintPage({
@@ -18,127 +19,146 @@ class ComplaintPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: SingleChildScrollView(
-          child: SizedBox(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Text(
-                    "Complaint ID: $ticketNumber",
-                    style: kContainerTextStyle.copyWith(
-                      fontSize: 14.0,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Text(
-                    "Project: $project",
-                    style: kContainerTextStyle.copyWith(
-                      fontSize: 15.0,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Text(
-                    "Bug: $complaint",
-                    style: kContainerTextStyle.copyWith(
-                      fontSize: 16.0,
-                    ),
-                  ),
-                ),
-
-                /// All states of the complaints are available as chips and they are each grayed out or colored
-                /// based on the state of the complaint
-                Row(
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: SingleChildScrollView(
+              child: SizedBox(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Padding(
                       padding: const EdgeInsets.all(20.0),
-                      child: Chip(
-                        label: Text(
-                          "Pending",
-                          style: kContainerTextStyle.copyWith(
-                            color: Colors.black,
-                          ),
+                      child: Text(
+                        "Complaint ID: $ticketNumber",
+                        style: kContainerTextStyle.copyWith(
+                          fontSize: 14.0,
                         ),
-                        backgroundColor:
-                            complaintState.name == ComplaintState.pending.name
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Text(
+                        "Project: $project",
+                        style: kContainerTextStyle.copyWith(
+                          fontSize: 15.0,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Text(
+                        "Bug: $complaint",
+                        style: kContainerTextStyle.copyWith(
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ),
+
+                    /// All states of the complaints are available as chips and they are each grayed out or colored
+                    /// based on the state of the complaint
+                    /// Enums are compared with names because the hashCodes change as new objects are created
+                    /// Since hashCodes are used in the equality comparison, the changing hashCodes can break it
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Chip(
+                            label: Text(
+                              "Pending",
+                              style: kContainerTextStyle.copyWith(
+                                color: Colors.black,
+                              ),
+                            ),
+                            backgroundColor: complaintState.name ==
+                                    ComplaintState.pending.name
                                 ? complaintState.associatedColor
                                 : Colors.grey.withAlpha(25),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Chip(
-                        label: Text(
-                          "Acknowledged",
-                          style: kContainerTextStyle.copyWith(
-                            color: Colors.black,
                           ),
                         ),
-                        backgroundColor: complaintState.name ==
-                                ComplaintState.acknowledged.name
-                            ? complaintState.associatedColor
-                            : Colors.grey.withAlpha(25),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Chip(
-                        label: Text(
-                          "In Progress",
-                          style: kContainerTextStyle.copyWith(
-                            color: Colors.black,
-                          ),
-                        ),
-                        backgroundColor: complaintState.name ==
-                                ComplaintState.inProgress.name
-                            ? complaintState.associatedColor
-                            : Colors.grey.withAlpha(25),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Chip(
-                        label: Text(
-                          "Completed",
-                          style: kContainerTextStyle.copyWith(
-                            color: Colors.black,
-                          ),
-                        ),
-                        backgroundColor:
-                            complaintState.name == ComplaintState.completed.name
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Chip(
+                            label: Text(
+                              "Acknowledged",
+                              style: kContainerTextStyle.copyWith(
+                                color: Colors.black,
+                              ),
+                            ),
+                            backgroundColor: complaintState.name ==
+                                    ComplaintState.acknowledged.name
                                 ? complaintState.associatedColor
                                 : Colors.grey.withAlpha(25),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Chip(
+                            label: Text(
+                              "In Progress",
+                              style: kContainerTextStyle.copyWith(
+                                color: Colors.black,
+                              ),
+                            ),
+                            backgroundColor: complaintState.name ==
+                                    ComplaintState.inProgress.name
+                                ? complaintState.associatedColor
+                                : Colors.grey.withAlpha(25),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Chip(
+                            label: Text(
+                              "Completed",
+                              style: kContainerTextStyle.copyWith(
+                                color: Colors.black,
+                              ),
+                            ),
+                            backgroundColor: complaintState.name ==
+                                    ComplaintState.completed.name
+                                ? complaintState.associatedColor
+                                : Colors.grey.withAlpha(25),
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    /// Below is an expanded uneditable text field title "Staff Notes and Work Plan" showing the notes from the staff
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 20.0,
+                        right: 20.0,
+                        top: 20.0,
+                      ),
+                      child: Text(
+                        "Staff Notes",
+                        style: kContainerTextStyle.copyWith(
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: constraints.maxHeight - 200 > 0
+                          ? constraints.maxHeight - 200
+                          : 0,
+                      decoration: BoxDecoration(
+                        color: lightAshyNavyBlue,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: buildNotes(),
                       ),
                     ),
                   ],
                 ),
-
-                /// Below is an expanded uneditable text field title "Staff Notes and Work Plan" showing the notes from the staff
-                Padding(
-                  padding: const EdgeInsets.only(
-                    left: 20.0,
-                    right: 20.0,
-                    top: 20.0,
-                  ),
-                  child: Text(
-                    "Staff Notes and Solution Plan",
-                    style: kContainerTextStyle.copyWith(
-                      fontSize: 18.0,
-                    ),
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
