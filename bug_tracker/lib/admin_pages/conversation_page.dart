a798b5431571cc468588f8ba0dc45336e8acb5d7
Brendan Chukwuemeka <chukwuemekachukwudi9@gmail.com>
2024-03-27 16:07:58 +0100

    complete conversation page placeholder

--- bug_tracker/lib/admin_pages/conversation_page.dart
+++ bug_tracker/lib/admin_pages/conversation_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:bug_tracker/utilities/constants.dart';
+import 'package:bug_tracker/ui_components/header_button.dart';
+import 'package:bug_tracker/ui_components/message_bubble.dart';
 
 class ConversationPage extends StatefulWidget {
   const ConversationPage({super.key});
@@ -9,7 +11,9 @@ class ConversationPage extends StatefulWidget {
 }
 
 class _ConversationPageState extends State<ConversationPage> {
-  /// add Leave conversation
+  ///TODO: add Leave conversation
+  final messageTextController = TextEditingController();
+  final scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +23,61 @@ class _ConversationPageState extends State<ConversationPage> {
         builder: (BuildContext context, BoxConstraints constraints) {
           return Padding(
             padding: const EdgeInsets.all(15.0),
-            child: ListView(),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: 50,
+                    controller: scrollController,
+                    itemBuilder: (BuildContext context, int index) {
+                      return MessageBubble(
+                          sender: "me",
+                          text: "mini me==========\nrre testing",
+                          isMe: (index % 2 == 0) ? true : false);
+                    },
+                  ),
+                ),
+                Container(
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      top: BorderSide(
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: messageTextController,
+                          onChanged: (value) {},
+                          decoration: const InputDecoration(
+                            contentPadding: EdgeInsets.symmetric(
+                              vertical: 10.0,
+                              horizontal: 20.0,
+                            ),
+                            hintText: 'Type your message here...',
+                            hintStyle: kContainerTextStyle,
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                      HeaderButton(
+                        screenIsWide: true,
+                        buttonText: "Send",
+                        onPress: () {
+                          /// add to db
+                          messageTextController.clear();
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           );
         },
       ),
---
+++ bug_tracker/lib/ui_components/message_bubble.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:bug_tracker/utilities/constants.dart';
+
+class MessageBubble extends StatelessWidget {
+  const MessageBubble(
+      {Key? key, required this.sender, required this.text, required this.isMe})
+      : super(key: key);
+
+  final String sender;
+  final String text;
+  final bool isMe;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment:
+            isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: [
+          Text(
+            sender,
+            style: kContainerTextStyle.copyWith(
+              color: Colors.white,
+              fontSize: 12.0,
+            ),
+          ),
+          Material(
+            elevation: 15.0,
+            shadowColor: Colors.black,
+            borderRadius: BorderRadius.only(
+              topLeft: isMe ? const Radius.circular(30.0) : Radius.zero,
+              topRight: isMe ? Radius.zero : const Radius.circular(30.0),
+              bottomLeft: const Radius.circular(30.0),
+              bottomRight: const Radius.circular(30.0),
+            ),
+            color: isMe == true ? Colors.lightBlueAccent : Colors.white,
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+              child: Text(
+                text,
+                style: kContainerTextStyle.copyWith(
+                  color: isMe ? Colors.white : Colors.black,
+                  fontSize: 15.0,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
