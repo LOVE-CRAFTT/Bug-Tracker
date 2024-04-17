7bcad8ce852492b8970be3139a161b7d35b202fc
Brendan Chukwuemeka <chukwuemekachukwudi9@gmail.com>
2024-04-18 00:47:16 +0100

    update task state setting during retrieval
    - reimplement Task.fromResultRow constructor to automatically set task as due today or overdue when necessary during database retrieval

--- bug_tracker/lib/utilities/task.dart
+++ bug_tracker/lib/utilities/task.dart
@@ -15,21 +15,41 @@ class Task {
     required this.isTeamLead,
   });
 
-  Task.fromResultRow({
+  // implemented as factory to allow for state change
+  // to dueToday or Overdue
+  factory Task.fromResultRow({
     required ResultRow taskRow,
     required Complaint associatedComplaint,
     required Staff assignedStaff,
-  }) : this(
-          id: taskRow['id'],
-          task: taskRow['task_name'],
-          taskState: TaskState.values.firstWhere(
-            (state) => state.title == taskRow['task_state'],
-          ),
-          associatedComplaint: associatedComplaint,
-          dueDate: DateTime.parse(taskRow['due_date'].toString()).toLocal(),
-          assignedStaff: assignedStaff,
-          isTeamLead: taskRow['is_team_lead'] == 1 ? true : false,
-        );
+  }) {
+    // Temporary value, might be overwritten
+    TaskState taskState = TaskState.values.firstWhere(
+      (state) => state.title == taskRow['task_state'],
+    );
+
+    // Overwrite the taskState based on the conditions if it is not completed
+    if (taskState != TaskState.completed) {
+      DateTime dueDate =
+          DateTime.parse(taskRow['due_date'].toString()).toLocal();
+      if (dueDate.day == DateTime.now().day &&
+          dueDate.month == DateTime.now().month &&
+          dueDate.year == DateTime.now().year) {
+        taskState = TaskState.dueToday;
+      } else if (dueDate.isBefore(DateTime.now())) {
+        taskState = TaskState.overdue;
+      }
+    }
+
+    return Task(
+      id: taskRow['id'],
+      task: taskRow['task_name'],
+      taskState: taskState,
+      associatedComplaint: associatedComplaint,
+      dueDate: DateTime.parse(taskRow['due_date'].toString()).toLocal(),
+      assignedStaff: assignedStaff,
+      isTeamLead: taskRow['is_team_lead'] == 1 ? true : false,
+    );
+  }
 
   final int id;
   final Complaint associatedComplaint;
